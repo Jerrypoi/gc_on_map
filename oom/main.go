48c83ee6db4e7563b86b47bde97e756e965167b8
Jerrypoi <jerry.poi.sz@gmail.com>
2021-03-23 15:02:42 +0800

    add oom example

---
+++ oom/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"sync"
+)
+
+var (
+	dataMap       = make(map[int64][]byte)
+	lock          = sync.Mutex{}
+	index   int64 = 0
+)
+
+func main() {
+	for {
+		dataMap[index] = make([]byte, 1024)
+		index++
+		if index%100000 == 0 {
+			log.Println(index)
+		}
+	}
+}
+
+/*func FreePtr(p unsafe.Pointer)
+
+func Free(v interface {}) {
+	FreePtr(unsafe.Pointer(reflect.ValueOf(v).Elem().Pointer()))
+}*/
